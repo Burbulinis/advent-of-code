ecb07d7f5d2336e53facf5fee24512b011d773b5
Burbulinis <martynasaleksandras19@gmail.com>
2024-12-02 13:54:58 +0200

    Day 2

--- src/main/java/me/burb/adventofcode/solutions/year2024/Day2Solution.java
+++ src/main/java/me/burb/adventofcode/solutions/year2024/Day2Solution.java
@@ -7,10 +7,13 @@ import java.util.Arrays;
 
 @AdventOfCodeSolution(year = 2024, day = 2, name = "Red-Nosed Reports", link = "https://adventofcode.com/2024/day/2")
 public class Day2Solution extends Solution {
+
     @Override
     public Object part1Solution() {
         return Arrays.stream(getInputLines())
-                .map(this::parseLine)
+                .map(a -> Arrays.stream(a.split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray())
                 .filter(this::isValidPartOne)
                 .count();
     }
@@ -18,29 +21,31 @@ public class Day2Solution extends Solution {
     @Override
     public Object part2Solution() {
         return Arrays.stream(getInputLines())
-                .map(this::parseLine)
+                .map(a -> Arrays.stream(a.split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray())
                 .filter(this::isValidPartTwo)
                 .count();
     }
 
-    private boolean isValidPartOne(String[] row) {
-        return isSafeRow(row, false);
+    private boolean isValidPartOne(int[] row) {
+        return isRowSafe(row, false);
     }
 
-    private boolean isValidPartTwo(String[] row) {
-        return isSafeRow(row, true);
+    private boolean isValidPartTwo(int[] row) {
+        return isRowSafe(row, true);
     }
 
-    private boolean isSafeRow(String[] row, boolean useDampener) {
-        boolean isIncreasing = Integer.parseInt(row[0]) < Integer.parseInt(row[1]);
+    private boolean isRowSafe(int[] row, boolean useDampener) {
+        boolean isIncreasing = row[0] < row[1];
 
         int problemDampener = 0;
 
         for (int i = 0; i < row.length-1; i++) {
-            int current = Integer.parseInt(row[i]);
-            int next = Integer.parseInt(row[i+1]);
+            int current = row[i];
+            int next = row[i+1];
 
-            if (!isLevelSafe(current, next, isIncreasing)) {
+            if (!isSafe(current, next, isIncreasing)) {
                 if (!useDampener) return false;
                 
                 problemDampener++;
@@ -50,7 +55,7 @@ public class Day2Solution extends Solution {
         return true;
     }
 
-    private boolean isLevelSafe(int a, int b, boolean increasing) {
+    private boolean isSafe(int a, int b, boolean increasing) {
         if (a == b) return false;
         if (a > b && increasing) return false;
         if (a < b && !increasing) return false;
@@ -59,7 +64,4 @@ public class Day2Solution extends Solution {
         return diff <= 3 && diff >= 1;
     }
 
-    private String[] parseLine(String line) {
-        return line.split(" ");
-    }
 }
